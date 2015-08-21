fdae8f49d03b61448475d5569fe136c06944f550
Jonathan <abrego.fc@gmail.com>
2015-08-20 22:51:07 -0500

    Ejercicio 1,2

--- Practicas/01/Practica01.rkt
+++ Practicas/01/Practica01.rkt
@@ -1,5 +1,17 @@
 #lang plai
 
+;Funciones auxiliares
+(define (mlength lst)
+  (cond
+    [(empty? lst) 0]
+    [else (+ 1 (mlength (cdr lst)))]))
+
+(define (sum lst)
+  (cond
+      [(empty? lst) 0]
+      [else (+ (car lst) (sum (cdr lst)))]))
+
+
 ;Ejercicio 1)
 (define (pow x n)
   (cond
@@ -8,6 +20,18 @@
 
 (test (pow 2 3) 8)
 (test (pow 10 0) 1)
-(test (pow 6 2) 100)
+(test (pow 10 2) 100)
 (test (pow 9 2) 81)
-(test (pow 3 3) 27)
+(test (pow 8 6) 27)
+
+;Ejercicio 2)
+(define (average lst)
+  (cond
+    [(zero? (sum lst)) 0]
+    [else (/ (sum lst) (mlength lst))]))
+
+(test (average '(5)) 5)
+(test (average '(3 2 6 2 1 7 2 1)) 3)
+(test (average '(10 7 13)) 10)
+(test (average '(1 1 1 1 1)) 2)
+(test (average '())0)
