f7e0f3c07bca54d7dcffe0648c8df0ee4d720906
firecold100 <c_m_a1990@live.com>
2015-08-26 18:24:22 -0500

    Update Practica1.rkt

--- Practicas/01/Practica1.rkt
+++ Practicas/01/Practica1.rkt
@@ -1,14 +1,12 @@
 #lang plai
 
-;Ejercicio 5
-
-
+;Ejercicio 6
 (define (mconcat lst1 lst2) 
   (cond
     [(empty? lst1) lst2]
     [else (cons (car lst1) (mconcat (cdr lst1) lst2))]))
 
-(test (mconcat '() '()) '())
+(test (mconcat '() '()) '()) ;The concation of two empty
 (test (mconcat '() '(1 2 3)) '(1 2 3))
 (test (mconcat '(1 2 3) '(1)) '(1 2 3 1))
 (test (mconcat '(4 5 6) '()) '(4 5 6))
@@ -21,6 +19,3 @@
     [(empty? lst) '()]
     [(empty? (cdr lst)) (car lst)]
     [else (getLast (cdr lst))]))
-
-
-;Ejercicio 
