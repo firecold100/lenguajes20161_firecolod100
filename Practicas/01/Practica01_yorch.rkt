3406137a634f2f7f1a6ca1603a9ecf6e94c7e5e5
yorchek <kuberjorg3n@hotmail.com>
2015-08-26 18:39:56 -0500

    quitando basura

--- Practicas/01/Practica01_yorch.rkt
+++ Practicas/01/Practica01_yorch.rkt
@@ -1,5 +1,7 @@
 #lang plai
 
+;Ejercicio 5
+
 (define (msum lst)
   (cond
       [(empty? lst) 0]
@@ -15,6 +17,9 @@
 ;(test (mreduce - '(100 50 25 12 6 3)) 66)
 ;(test (mreduce + '(1 2 3 4 5 6 7 8 9 10)) 55)
 
+
+;Ejercicio 7
+
 (define (mmap func lst)
   (cond
     [(empty? lst) '()]
@@ -24,6 +29,9 @@
 ;(test (mmap car '((1 2 3) (4 5 6) (7 8 9))) '(1 4 7))
 ;(test (mmap cdr '((1 2 3) (4 5 6) (7 8 9))) '((2 3) (5 6) (8 9)))
 ;(test (mmap car (cdr '((1 2 3) (4 5 6) (7 8 9)))) '(4 7))
+
+
+;Ejercicio 9
 
 (define (myany? pred lst)
   (cond
@@ -35,3 +43,16 @@
 ;(test (myany? number? '(a b c d 1)) #t)
 ;(test (myany? symbol? '(1 2 3 4)) #f)
 ;(test (myany? symbol? '(1 2 + 4)) #t)
+;(test (myany? empty? '()) #t)
+
+
+;Ejercicio 11
+
+(define (mpowerset lst)
+  (cond
+    [(empty? lst) '()]
+    [else (construye lst lst)]))
+
+(define (construye lst1 lst2))
+
+(test (mpowerset '(1 2)) '((1 2) (2) (1) ()))
