6ff4166756359cecb75188cb9d05ba74151d64a0
yorchek <kuberjorg3n@hotmail.com>
2015-08-26 17:32:06 -0500

    Ejercicio 5 subido por Alan

---
+++ Practicas/01/Practica1.rkt
@@ -0,0 +1,26 @@
+#lang plai
+
+;Ejercicio 5
+
+
+(define (mconcat lst1 lst2) 
+  (cond
+    [(empty? lst1) lst2]
+    [else (cons (car lst1) (mconcat (cdr lst1) lst2))]))
+
+(test (mconcat '() '()) '())
+(test (mconcat '() '(1 2 3)) '(1 2 3))
+(test (mconcat '(1 2 3) '(1)) '(1 2 3 1))
+(test (mconcat '(4 5 6) '()) '(4 5 6))
+(test (mconcat '(1 2 3) '(4 5 6)) '(1 2 3 4 5 6))
+
+;Function which takes a list 
+;and give us the last element of it. (list) -> (list)
+(define (getLast lst)
+  (cond
+    [(empty? lst) '()]
+    [(empty? (cdr lst)) (car lst)]
+    [else (getLast (cdr lst))]))
+
+
+;Ejercicio 
