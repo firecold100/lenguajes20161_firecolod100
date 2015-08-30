8819d7329d644e01808e6c1ea2022ff8bada0bfd
firecold <c_m_a1990@live.com>
2015-08-29 21:32:18 -0500

    Subo ejercicio 10 mevery? con pruebas unitarias

--- Practicas/01/Practica1.rkt
+++ Practicas/01/Practica1.rkt
@@ -38,3 +38,20 @@
 (test (mfilter (lambda (u) (< u 5)) '(12 32 1 2 3 5)) '(1 2 3 ))
 (test (mfilter (lambda (h) (eq? #t h)) '(#f #f #t #t #t)) '(#t #t #t))
 
+;Ejercicio 10
+
+(define (mevery? predicate lst)
+  (cond
+    ;If the list is empty, then we return true by vacuity
+    [(empty? lst) #t]
+    ;If the predicate is true applied to the head of the list, then we continue recursively searching
+    ;the element that doesn't returns true
+    [(predicate (car lst)) (mevery? predicate (cdr lst))]
+    ;Else we return false, because we found an element that doesn't meets the requeriments
+    [else #f]))
+
+(test (mevery? (lambda (x) (= (modulo x 3) 1)) '(1 4 7 10 13) ) #t)
+(test (mevery? (lambda (x) (eq? "pikachu" x)) '("pikachu" "raichu") ) #f)
+(test (mevery? number? '(1 2 3 4)) #t)
+(test (mevery? (lambda (x) (empty? x)) '('() '() '())) #f) ;It's false because is not empty the list.
+(test (mevery? string? '("charizar" "squirtle" "haunter" "combee")) #t)
