70fb97b7c144c04151a8de8b32c82883cd864be2
firecold <c_m_a1990@live.com>
2015-09-13 13:14:20 -0500

    Corregido ejercicio 1

--- Practicas/practica3/practica3.rkt
+++ Practicas/practica3/practica3.rkt
@@ -21,7 +21,7 @@
                                          (- (+ rest (* range (+ 0.5 (* 0.1 i)))) 1))))])
                  (aux (+ i 1) range rest lst))]
       [(= i 5 )(let ([lst (append lstzones (list (maximum (+ rest (* range (+ 0.5 (* 0.1 (- i 1)))))  
-                                         (- (+ rest (* range (+ 0.5 (* 0.1 i)))) 1))))])
+                                         (+ rest (* range (+ 0.5 (* 0.1 i)))) )))])
                  lst)]));End of auxiliar function
   (cond
     [(not (number? rest)) error "The first param is not of type number"]
@@ -34,35 +34,35 @@
  (fat-burning 128.0 140.0)
  (aerobic 141.0 153.0)
  (anaerobic 154.0 166.0)
- (maximum 167.0 179.0)))
+ (maximum 167.0 180.0)))
 (test (zones 60 160) (list
  (resting 60 109.0)
  (warm-up 110.0 119.0)
  (fat-burning 120.0 129.0)
  (aerobic 130.0 139.0)
  (anaerobic 140.0 149.0)
- (maximum 150.0 159.0)))
+ (maximum 150.0 160.0)))
 (test (zones 50 170) (list
  (resting 50 109.0)
  (warm-up 110.0 121.0)
  (fat-burning 122.0 133.0)
  (aerobic 134.0 145.0)
  (anaerobic 146.0 157.0)
- (maximum 158.0 169.0)))
+ (maximum 158.0 170.0)))
 (test (zones 40 200) (list
  (resting 40 119.0)
  (warm-up 120.0 135.0)
  (fat-burning 136.0 151.0)
  (aerobic 152.0 167.0)
  (anaerobic 168.0 183.0)
- (maximum 184.0 199.0)))
+ (maximum 184.0 200.0)))
 (test (zones 60 130) (list
  (resting 60 94.0)
  (warm-up 95.0 101.0)
  (fat-burning 102.0 108.0)
  (aerobic 109.0 115.0)
  (anaerobic 116.0 122.0)
- (maximum 123.0 129.0)))
+ (maximum 123.0 130.0)))
 
 ;Ejercicio 2 get-zone
 (define my-zones (zones 50 180));Constant defined for the exercise
