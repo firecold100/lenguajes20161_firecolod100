1b030c3cf3d4f46032ebc73fda6948b00fbff8f3
Jonathan <abrego.fc@gmail.com>
2015-11-28 20:34:08 -0600

    Lector cvs

---
+++ Practicas/practica6/Graph.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import csv
+  
+reader = csv.reader(open('graph.csv', 'rb'))
+tipo= next(reader)
+next(reader, None)
+print(tipo)
+for index,row in enumerate(reader):
+    print '------------'
+    print 'Orgien: ' + row[0] + ', Destino: ' + row[1] + ', Peso: ' + row[2]    
