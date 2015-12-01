963ed87fb4bcf0023149c75e57de6becb73b65b3
Jonathan <abrego.fc@gmail.com>
2015-12-01 14:08:14 -0600

    CSV

--- Practicas/practica6/GraphReader.py
+++ Practicas/practica6/GraphReader.py
@@ -4,15 +4,24 @@ import csv
 import json   
 from xml.dom import minidom
 
-def readCVS():
-	reader = csv.reader(open('graph.csv', 'rb'))
+def readCSV():
+	reader = csv.reader(open('graph.csv', 'r'))
 	tipo= next(reader, None)	
 	if tipo ==['direct=1']:
 		print('Dirigida')
 	else:
 		print('No dirigida')
+	pesos = []
+	origen = []
+	destino = []
 	for row in enumerate(reader):
-		print('Orgien: ' + row[1][0] + ', Destino: ' + row[1][1] + ', Peso: '+ row[1][2])
+		print('Orgien: ' +row[1][0]+', Destino: '+row[1][1]+', Peso: '+row[1][2])
+		origen.append(row[1][0]) 		
+		destino.append(row[1][1])
+		pesos.append(row[1][2])
+	print(origen[0])
+	print(destino[0])
+	print(pesos[0])
 
 def readJSON():
 	with open('graph.json',encoding='utf-8') as data_file:
