d969ed221a230d3f97aec0a522077268f819c3c0
Jonathan <abrego.fc@gmail.com>
2015-12-01 11:50:54 -0600

    lectores

--- Practicas/practica6/GraphReader.py
+++ Practicas/practica6/GraphReader.py
@@ -2,13 +2,26 @@
 
 import csv
 import json   
+from xml.dom import minidom
 
 def readCVS():
 	reader = csv.reader(open('graph.csv', 'rb'))
-	next(reader, None)
+	tipo= next(reader, None)	
+	if tipo ==['direct=1']:
+		print('Dirigida')
+	else:
+		print('No dirigida')
 	for row in enumerate(reader):
-		print 'Orgien: ' + row[1][0] + ', Destino: ' + row[1][1] + ', Peso: '+ row[1][2]
+		print('Orgien: ' + row[1][0] + ', Destino: ' + row[1][1] + ', Peso: '+ row[1][2])
 
 def readJSON():
-	prueba =  json.loads(open('graph.json').read())
-	print(prueba)
+	with open('graph.json',encoding='utf-8') as data_file:
+		data =  json.loads(data_file.read())
+	print(data)
+
+def readXML():
+	doc = minidom.parse("graph.xml")
+	vertexs = doc.getElementsByTagName("vertex")
+	for vertex in vertexs:
+		sid = vertex.getAttribute("label")
+		print("label:%s " %(sid))
